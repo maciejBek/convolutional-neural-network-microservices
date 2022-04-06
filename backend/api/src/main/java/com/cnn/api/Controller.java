4595174a7aabb325278da47b20dcb624bec67d4d
maciejBek <maciek8928@o2.pl>
2022-04-06 22:53:36 +0200

    add file

---
+++ backend/api/src/main/java/com/cnn/api/Controller.java
@@ -0,0 +1,17 @@
+package com.cnn.api;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+    @GetMapping
+    public String getTest() {
+        return "API";
+    }
+}
