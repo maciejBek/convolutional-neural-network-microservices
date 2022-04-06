65b396bf76bd628ec67562be14b0196127e2442f
maciejBek <maciek8928@o2.pl>
2022-04-06 22:54:36 +0200

    add file

---
+++ backend/data-access-persistance/src/main/java/com/cnn/dataaccesspersistance/DataAccessPersistanceApplication.java
@@ -0,0 +1,13 @@
+package com.cnn.dataaccesspersistance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAccessPersistanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataAccessPersistanceApplication.class, args);
+    }
+
+}
