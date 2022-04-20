ea059274e34a6d73b3d29e97a761074ccfafffc9
Filip50n <nic990000@gmail.com>
2022-04-20 21:04:36 +0200

    stylistic changes

--- frontend/src/Component/Photo.js
+++ frontend/src/Component/Photo.js
@@ -2,13 +2,25 @@ import React from 'react';
 import axios from 'axios';
 
 const Photo = () => {
+
+  const [statephoto, setStatephoto] = React.useState("None")
+  const [img, setImg] = React.useState();
+
   // a local state to store the currently selected file.
   const [selectedFile, setSelectedFile] = React.useState(null);
 
+  const onImageChange = (e) => {
+    const [file] = e.target.files;
+    setImg(URL.createObjectURL(file));
+  };
+
   const handleSubmit = (event) => {
+    setStatephoto("")
     event.preventDefault()
     const formData = new FormData();
     formData.append("image", selectedFile);
+
+    console.log(selectedFile)
 
     axios({
       method: "post",
@@ -18,8 +30,9 @@ const Photo = () => {
     })
       .then(function (response) {
           //handle success
-          console.log(response)
-          console.log(response.data.object)
+          // console.log(response)
+          // console.log(response.data.object)
+          setStatephoto(response.data.object)
       })
       .catch(function (response) {
           //handle error
@@ -30,13 +43,18 @@ const Photo = () => {
 
   const handleFileSelect = (event) => {
     setSelectedFile(event.target.files[0])
+    setImg(URL.createObjectURL(event.target.files[0]));
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="file" onChange={handleFileSelect}/>
-      <input type="submit" value="Wyślij plik" />
-    </form>
+    <div id="Photo">
+      <img id="Img" src={img} alt="" />
+      <form id="Form" onSubmit={handleSubmit}>
+        <input type="file" onChange={handleFileSelect}/>
+        <input type="submit" value="Wyślij plik" />
+      </form>
+      <div id="Ans1">Na zdjęciu wykryto: <div id="Ans2">{statephoto}</div></div>
+    </div>
   )
 };
 
