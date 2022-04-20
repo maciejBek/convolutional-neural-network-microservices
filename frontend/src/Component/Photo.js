78e6ae07d02d36ac637d2252879ecafac1e28770
Filip50n <nic990000@gmail.com>
2022-04-20 16:29:13 +0200

    Response correct

--- frontend/src/Component/Photo.js
+++ frontend/src/Component/Photo.js
@@ -16,6 +16,7 @@ const Photo = () => {
         data: formData,
         headers: { "Content-Type": "multipart/form-data" },
       });
+      console.log(response)
       console.log(response.data)
     } catch(error) {
       console.log(error)
