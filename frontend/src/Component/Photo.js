a7022ce0bf303e925a1cc60ec4c1421ab817aa34
Filip50n <nic990000@gmail.com>
2022-04-20 16:06:22 +0200

    send Photo to server

---
+++ frontend/src/Component/Photo.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import axios from 'axios';
+
+const Photo = () => {
+  // a local state to store the currently selected file.
+  const [selectedFile, setSelectedFile] = React.useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const formData = new FormData();
+    formData.append("selectedFile", selectedFile);
+    try {
+      const response = axios({
+        method: "post",
+        url: "http://192.168.0.178:5000/images",
+        data: formData,
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  const handleFileSelect = (event) => {
+    setSelectedFile(event.target.files[0])
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="file" onChange={handleFileSelect}/>
+      <input type="submit" value="Upload File" />
+    </form>
+  )
+};
+
+export default Photo;
